c41d58b0e8b7c65cb9920473a0713f9e80459f66
Josue Kazimoto <kazimotojosue@gmail.com>
2025-12-07 13:18:11 +0200

    Ajout des fonctions compter et ajouter des noeuds

--- main.py
+++ main.py
@@ -14,5 +14,30 @@ table_matiere = {
 
 def afficher_preordre(arbre, niveau=0):
     for titre, enfants in arbre.items():
-        print("  " * niveau + "- " + titre)
+        print("  " * niveau + "|-- " + titre)
         afficher_preordre(enfants, niveau + 1)
+
+def compter_noeuds(arbre):
+    total = 0
+    for enfants in arbre.values():
+        total += 1  # Compter le noeud courant
+        total += compter_noeuds(enfants)
+          # Compter les noeuds enfants
+    return total
+
+def ajouter_element(arbre, cible, nouveau):
+    for titre, enfants in arbre.items():
+        if titre == cible:
+            enfants[nouveau] = {}
+            return True  # insertion réussie
+
+        # sinon on continue à chercher dans les sous-niveaux
+        if ajouter_element(enfants, cible, nouveau):
+            return True
+
+    return False  # cible non trouvée
+
+
+afficher_preordre(table_matiere)
+nombre = compter_noeuds(table_matiere)
+print(f"\nNombre total de sections et sous-sections : {nombre}")
